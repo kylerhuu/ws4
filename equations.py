c7dfa0319027bd51231351a8221a7613cac3c95e
Kyler Hu <kylerhu1549@gmail.com>
2026-02-05 15:52:10 -0800

    addedequations

--- equations.py
+++ equations.py
@@ -1,2 +1,5 @@
 def add(x, y):
     return x + y
+
+def multiply(x,y,z):
+    return x*y*z
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import equations
 
 x = equations.add(6, 7)
 print(x)
+
+y= equations.multiply(6,7,8)
+print("the product of 6 and seven is",y)
